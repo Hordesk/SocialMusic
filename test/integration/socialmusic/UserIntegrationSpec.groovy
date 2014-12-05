b10866bec11759d438e7cc418ab78c4c7e61325f
Rémi Benoit <r3m1.benoit@gmail.com>
2014-12-05 12:43:18 +0100

    Failing integration test for register user

---
+++ test/integration/socialmusic/UserIntegrationSpec.groovy
@@ -0,0 +1,51 @@
+package socialmusic
+
+import grails.test.spock.IntegrationSpec
+import spock.lang.*
+
+/**
+ *
+ */
+class UserIntegrationSpec extends Specification{
+
+    def userService
+
+    def populateValidParams(params) {
+        assert params != null
+        params["username"] = 'user2'
+        params["password"] = 'mdp2'
+        params["id"] = 1
+    }
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "should register user"() {
+
+        //FIXME: test not working, controller.response properties are null
+//        given:"a controller and a valid user"
+//        def controller = new UserController()
+//        controller.userService = userService
+//
+//        populateValidParams(controller.params)
+//        def user = new User(controller.params)
+//
+//        when: "user is saved"
+//        controller.save(user)
+//
+//        then:"A redirect is issued to the show action"
+//        controller.response.redirectedUrl == '/user/show/1'
+//        controller.flash.message != null
+//
+//        User.count() == 1
+//        SecUserSecRole.count() == 1
+//
+//        User.first().getAuthorities().size() == 1
+//        User.first().getAuthorities().first() == SecRole.findByAuthority("ROLE_USER")
+
+        assert true
+    }
+}
