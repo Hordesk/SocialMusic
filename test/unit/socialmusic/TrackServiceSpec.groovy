e00370c63f8345a9ee200a8107b5174d3f6417ee
Rémi Benoit <r3m1.benoit@gmail.com>
2014-12-08 12:07:51 +0100

    TRackService unit test for adding a track

--- test/unit/socialmusic/TrackServiceSpec.groovy
+++ test/unit/socialmusic/TrackServiceSpec.groovy
@@ -1,6 +1,5 @@
 package socialmusic
 
-import grails.plugin.springsecurity.SpringSecurityService
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import org.springframework.transaction.PlatformTransactionManager
@@ -13,45 +12,33 @@ import spock.lang.Specification
 @TestFor(TrackService)
 @Mock([User, Track, Grade])
 class TrackServiceSpec extends Specification {
-//
-////    SpringSecurityService springSecurityService
-//
-//
-//    def setup() {
-//        // Workaround for GRAILS-10538
-//        service.transactionManager = Mock(PlatformTransactionManager) { getTransaction(_) >> Mock(TransactionStatus) }
-//        def springSecurityService = Mock(SpringSecurityService)
-//        springSecurityService.loggedIn >> true
-//        User loggedInUser = Mock(User)
-//        User user =new User(username:"User",password: "password", id: 1)
-//        user.save flush:true
-//
-//        assert User.count() == 1
-//        def userService = Mock(UserService)
-//        userService.currentUser >> user
-//        springSecurityService.principal >> {id -> 1}
-////        springSecurityService.currentUser.username >> "User"
-////        springSecurityService.currentUser.password >> "password"
-//
-//        service.userService = userService
-//
-//    }
-//
-//    void "Test the track service correctly persists an instance of track and grade"() {
-//        given: "a valid track instance and authentified user"
-//
-//
-//        def track = new Track(title:"titleTest",album:"albumTest",artist:"artistTest")
-//
-//
-//        when:"The save action is executed with a valid instance"
-//        service.addTrack(track)
-//
-//        then:"An instance is created and associated with USER role"
-//        service.addTrack(track).class == Grade.class
-////        Track.count() == 1
-////        Grade.count() == 1
-//
-//
-//    }
+
+    def setup() {
+        // Workaround for GRAILS-10538
+        service.transactionManager = Mock(PlatformTransactionManager) { getTransaction(_) >> Mock(TransactionStatus) }
+
+        User user =new User(username:"User",password: "password", id: 1)
+
+        def userService = Mock(UserService) {
+            currentUser() >> user
+        }
+        service.userService = userService
+    }
+
+    void "Test the track service correctly persists an instance of track and grade"() {
+
+        given: "a valid track instance and authentified user"
+        def track = new Track(title:"titleTest",album:"albumTest",artist:"artistTest")
+
+        when:"The save action is executed with a valid instance"
+        service.addTrack(track)
+
+        then:"A Track and Grade are created"
+        Track.count() == 1
+        Grade.count() == 1
+
+        then:"The track has one grade of value 0"
+        Track.first().grades.size() == 1
+        Track.first().grades.first().grade == 0
+    }
 }
