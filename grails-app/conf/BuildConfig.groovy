01d01141eb3d725164aada226a042d37c74e2c15
Rémi Benoit <r3m1.benoit@gmail.com>
2014-12-10 14:56:33 +0100

    Exclude generated code from coverage

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -98,4 +98,8 @@ grails.project.dependency.resolution = {
         //compile ":coffee-asset-pipeline:1.5.0"
         //compile ":handlebars-asset-pipeline:1.3.0.1"
     }
+
+    coverage {
+        exclusions = ["**/SecUserSecRole*", "**/SecUser*"]
+    }
 }
