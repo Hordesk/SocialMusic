f5df5e56aa917341ed94b0c6c7190024389991d3
Rémi Benoit <r3m1.benoit@gmail.com>
2014-12-10 19:02:47 +0100

    Set DISPLAY variable when launching firefox

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -4,8 +4,14 @@
 	See: http://www.gebish.org/manual/current/configuration.html
 */
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxBinary
+
 
 driver = {
+    final File firefoxPath = new File(System.getProperty("MYAPP.firefox.path", "/usr/bin/firefox"))
+    FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath)
+    firefoxBinary.setEnvironmentProperty("DISPLAY", ":99.0")
+    driverInstance = new FirefoxDriver(firefoxBinary, null)
     def driver = new FirefoxDriver()
     driver
 }
