e09cfeef0577b6755344ad80bb74da35d3d9ab05
elmahdibenhadda <elmahdi.benhadda@gmail.com>
2014-12-11 14:27:36 +0100

    Test GradeSpec : Correction

--- test/unit/socialmusic/GradeSpec.groovy
+++ test/unit/socialmusic/GradeSpec.groovy
@@ -10,31 +10,4 @@ import spock.lang.Unroll
 @TestFor(Grade)
 class GradeSpec extends Specification {
 
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    @Unroll
-    void "username of user should be unique : #ausername1, #ausername2"() {
-        given: "Two users"
-        def u1 = new User(username: "ausername1", password: "password1")
-        def t1 = new Track(title: "aTitle", artist: "anArtist", album: "anAlbum")
-        def grade = new Grade(user:u1,track: t1,grade:agrade)
-
-        when: "User1 then user2 are validated"
-        def tostring1 =grade.tostring()
-
-
-        then: "the results are coherent"
-        tostring1 == expected
-
-
-        where:
-        agrade      | expected
-        "0" | "duplicateusername"
-        "1"                | "1"
-        "-1"           | "-1"
-    }
 }
